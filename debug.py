d1ff56406020f90d9570d3f01419ab9040ee920c
noah <noah@chromakey.io>
2020-08-06 11:06:13 -0700

    pushing research-id into .env

--- debug.py
+++ debug.py
@@ -1,17 +1,20 @@
 from subject.models import Subject
+from settings import RESEARCH_ID
 
 async def bootstrap():
+    """ research_id needs to match YOUR associated authentication key """
+
     sub = Subject()
-    sub.research_id = '5f18d06fcfcc010013c0c3f7'
+    sub.research_id = RESEARCH_ID
     sub.identifier = "Fred Flintstone"
     await sub.save()
 
     sub = Subject()
-    sub.research_id = '5f18d06fcfcc010013c0c3f7'
+    sub.research_id = RESEARCH_ID
     sub.identifier = "Barney Rubble"
     await sub.save()
 
     sub = Subject()
-    sub.research_id = '5f18d06fcfcc010013c0c3f7'
+    sub.research_id = RESEARCH_ID
     sub.identifier = "Betty Boop"
     await sub.save()
