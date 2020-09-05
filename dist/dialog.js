ac3e3a85ace4015fd5eb9cbcfebb2136a069aac5
noah <noah@chromakey.io>
2020-09-04 23:05:34 -0700

    a boatloead of JS refractoring

--- dist/dialog.js
+++ dist/dialog.js
@@ -1,34 +1,8 @@
-import { L as LitElement, f as css, h as html } from './index.js';
-import { ErrorMessage } from './utils.js';
-
-class SubjectDialog extends LitElement {
-
-    static get styles() {
-        return css`
+import{L as t,f as e,h as i}from"./index.js";import{ErrorMessage as d}from"./utils.js";class s extends t{static get styles(){return e`
             mwc-textfield {
                 width:100%;
             }
-        `;
-    }
-    constructor (options) {
-        super();
-        this._token = options.token;
-        this.subjects_list = options.subjects_list;
-    }
-
-    get identity() {
-        return this.shadowRoot.getElementById('identity').value
-    }
-
-    get birthdate() {
-        return this.shadowRoot.getElementById('birthdate').value
-    }
-
-    render() {
-        const drawer = document.getElementsByTagName('mwc-drawer')[0];
-        drawer.open = false;
-
-        return html`
+        `}constructor(t){super(),this._token=t.token,this.subjects_list=t.subjects_list}get identity(){return this.shadowRoot.getElementById("identity").value}get birthdate(){return this.shadowRoot.getElementById("birthdate").value}render(){return document.getElementsByTagName("mwc-drawer")[0].open=!1,i`
             <mwc-dialog id="id-dialog" heading="Identity" open>
                 <p>Create a new subject:</p>
                 <p>
@@ -39,38 +13,5 @@ class SubjectDialog extends LitElement {
                 </p>
                 <mwc-button id="submit-id" @click=${this.handler} slot="primaryAction">Confirm</mwc-button>
                 <mwc-button id="cancel-id" @click=${this.destroy} slot="secondaryAction" dialogAction="close">Cancel</mwc-button>
-            </mwc-dialog>`;
-    }
-
-    async handler() {
-        const data = JSON.stringify({
-            'identity': this.identity,
-            'birthdate': this.birthdate
-        });
-
-        const response = await fetch('/subject', {
-                method: 'POST',
-                headers: {
-                    Authorization: 'Bearer: ' + this._token
-                },
-                body: data
-        });
-
-        if (response.status == 200) {
-            const data = await response.json();
-        } else {
-            const error = new ErrorMessage({'message':"Error Saving Subject"});
-            document.getElementById('content').appendChild(error);
-        }
-        await this.destroy();
-    }
-
-    async destroy() {
-        this.subjects_list.handler();
-        this.remove();
-    }
-}
-window.customElements.define('subject-dialog', SubjectDialog);
-
-export { SubjectDialog };
+            </mwc-dialog>`}async handler(){const t=JSON.stringify({identity:this.identity,birthdate:this.birthdate}),e=await fetch("/subject",{method:"POST",headers:{Authorization:"Bearer: "+this._token},body:t});if(200==e.status){await e.json()}else{const t=new d({message:"Error Saving Subject"});document.getElementById("content").appendChild(t)}await this.destroy()}async destroy(){this.subjects_list.handler(),this.remove()}}window.customElements.define("subject-dialog",s);export{s as SubjectDialog};
 //# sourceMappingURL=dialog.js.map
